f80758a5050f015f4cb07fa35860569c72cb5162
kartoffelknoedeln <11416589+kartoffelknoedeln@users.noreply.github.com>
2020-08-06 01:28:14 -0500

    Update markov-colours.py

--- markov-colours.py
+++ markov-colours.py
@@ -1,8 +1,7 @@
 import random
 import csv
 
-class Markov(object):
-
+class Markov:
     def __init__(self, counter = 0, black = 0, red = 0, blue = 0, white = 0, yellow = 0):
         self.counter = counter
         self.black = black
@@ -18,30 +17,138 @@ class Markov(object):
         savefile.write(str(white) + ',')
         savefile.write(str(yellow) + '\n')
 
-    def markov_chain(self, counter, black, red, blue, white, yellow):
+    def black_colour(self, counter, black, red, blue, white, yellow):
         randnum = random.randint(1, 101)
         blank_string = ''
         if counter < 100:
-            if randnum <= 10:
+            if randnum <= 30:
                 counter += 1
                 black += 1
-                self.markov_chain(counter, black, red, blue, white, yellow)
-            elif 11 <= randnum <= 20:
+                self.black_colour(counter, black, red, blue, white, yellow)
+            elif 31 <= randnum <= 35:
                 counter += 1
                 red += 1
-                self.markov_chain(counter, black, red, blue, white, yellow)
-            elif 20 <= randnum <= 30:
+                self.red_colour(counter, black, red, blue, white, yellow)
+            elif 36 <= randnum <= 40:
                 counter += 1
                 blue += 1
-                self.markov_chain(counter, black, red, blue, white, yellow)
-            elif 30 <= randnum <= 90:
+                self.blue_colour(counter, black, red, blue, white, yellow)
+            elif 41 <= randnum <= 95:
                 counter += 1
                 white += 1
-                self.markov_chain(counter, black, red, blue, white, yellow)
+                self.white_colour(counter, black, red, blue, white, yellow)
             else:
                 counter += 1
                 yellow += 1
-                self.markov_chain(counter, black, red, blue, white, yellow)
+                self.yellow_colour(counter, black, red, blue, white, yellow)
+        else:
+            self.write_to_csv(black, red, blue, white, yellow)
+
+    def red_colour(self, counter, black, red, blue, white, yellow):
+        randnum = random.randint(1, 101)
+        blank_string = ''
+        if counter < 100:
+            if randnum <= 5:
+                counter += 1
+                black += 1
+                self.black_colour(counter, black, red, blue, white, yellow)
+            elif 6 <= randnum <= 35:
+                counter += 1
+                red += 1
+                self.red_colour(counter, black, red, blue, white, yellow)
+            elif 36 <= randnum <= 40:
+                counter += 1
+                blue += 1
+                self.blue_colour(counter, black, red, blue, white, yellow)
+            elif 41 <= randnum <= 95:
+                counter += 1
+                white += 1
+                self.white_colour(counter, black, red, blue, white, yellow)
+            else:
+                counter += 1
+                yellow += 1
+                self.yellow_colour(counter, black, red, blue, white, yellow)
+        else:
+            self.write_to_csv(black, red, blue, white, yellow)
+
+    def blue_colour(self, counter, black, red, blue, white, yellow):
+        randnum = random.randint(1, 101)
+        blank_string = ''
+        if counter < 100:
+            if randnum <= 5:
+                counter += 1
+                black += 1
+                self.black_colour(counter, black, red, blue, white, yellow)
+            elif 6 <= randnum <= 10:
+                counter += 1
+                red += 1
+                self.red_colour(counter, black, red, blue, white, yellow)
+            elif 11 <= randnum <= 40:
+                counter += 1
+                blue += 1
+                self.blue_colour(counter, black, red, blue, white, yellow)
+            elif 41 <= randnum <= 95:
+                counter += 1
+                white += 1
+                self.white_colour(counter, black, red, blue, white, yellow)
+            else:
+                counter += 1
+                yellow += 1
+                self.yellow_colour(counter, black, red, blue, white, yellow)
+        else:
+            self.write_to_csv(black, red, blue, white, yellow)
+
+    def white_colour(self, counter, black, red, blue, white, yellow):
+        randnum = random.randint(1, 101)
+        blank_string = ''
+        if counter < 100:
+            if randnum <= 5:
+                counter += 1
+                black += 1
+                self.black_colour(counter, black, red, blue, white, yellow)
+            elif 6 <= randnum <= 10:
+                counter += 1
+                red += 1
+                self.red_colour(counter, black, red, blue, white, yellow)
+            elif 11 <= randnum <= 15:
+                counter += 1
+                blue += 1
+                self.blue_colour(counter, black, red, blue, white, yellow)
+            elif 16 <= randnum <= 95:
+                counter += 1
+                white += 1
+                self.white_colour(counter, black, red, blue, white, yellow)
+            else:
+                counter += 1
+                yellow += 1
+                self.yellow_colour(counter, black, red, blue, white, yellow)
+        else:
+            self.write_to_csv(black, red, blue, white, yellow)
+
+    def yellow_colour(self, counter, black, red, blue, white, yellow):
+        randnum = random.randint(1, 101)
+        blank_string = ''
+        if counter < 100:
+            if randnum <= 5:
+                counter += 1
+                black += 1
+                self.black_colour(counter, black, red, blue, white, yellow)
+            elif 6 <= randnum <= 10:
+                counter += 1
+                red += 1
+                self.red_colour(counter, black, red, blue, white, yellow)
+            elif 11 <= randnum <= 15:
+                counter += 1
+                blue += 1
+                self.blue_colour(counter, black, red, blue, white, yellow)
+            elif 16 <= randnum <= 70:
+                counter += 1
+                white += 1
+                self.white_colour(counter, black, red, blue, white, yellow)
+            else:
+                counter += 1
+                yellow += 1
+                self.yellow_colour(counter, black, red, blue, white, yellow)
         else:
             self.write_to_csv(black, red, blue, white, yellow)
            
@@ -52,7 +159,7 @@ if __name__ == '__main__':
     
     for i in range(1, 10):
         savefile.write(str(i) + ',')
-        markov.markov_chain(markov.counter, markov.black, markov.red, markov.blue, markov.white, markov.yellow)
+        markov.black_colour(markov.counter, markov.black, markov.red, markov.blue, markov.white, markov.yellow)
 
     savefile.close()
     print('Done.')
